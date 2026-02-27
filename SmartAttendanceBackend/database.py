0f5bfacfa78fe4ae3c7966d050af5bae429debcf
Aditya6392070633 <Deepak954807@gmail.com>
2026-02-27 11:51:30 +0530

    Add Neon database and requirements

--- SmartAttendanceBackend/database.py
+++ SmartAttendanceBackend/database.py
@@ -1,12 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/attendance_db")
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_nse7DbLAEJ6K@ep-dark-forest-aiw03azg-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
